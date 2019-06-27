07b677532993e9197e98556e2135c2f8e3af5882
Matthew Kaplan <kaplan.matthew.p@gmail.com>
2019-06-27 05:32:16 -0600

    Copy email to clipboard

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,9 +4,22 @@ import { Link } from "react-scroll";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import { faChevronDown, faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { CopyToClipboard } from "react-copy-to-clipboard";
 
 export default class Home extends Component {
+  state = { isNotCopied: true };
+
+  onCopy = () => {
+    this.setState({ isNotCopied: false });
+  };
+  
+  hideMessage = () => {
+    this.setState({isNotCopied: true});
+  }
+
   render() {
+    const { isNotCopied } = this.state;
+    console.log(isNotCopied)
     return (
       <div className="home-component">
         <div className="overlay" />
@@ -37,15 +50,28 @@ export default class Home extends Component {
                   size="2x"
                 />
               </a>
-              <FontAwesomeIcon
-                icon={faEnvelope}
-                className="github-icon"
-                size="2x"
-              />
+              <CopyToClipboard
+                onCopy={this.onCopy}
+                text={"Kaplan.Matthew.P@gmail.com"}
+                onMouseLeave={() => (setTimeout(this.hideMessage, 2000))}
+              >
+                <FontAwesomeIcon
+                  icon={faEnvelope}
+                  className="envelope-icon"
+                  size="2x"
+                />
+              </CopyToClipboard>
             </div>
           </div>
+          <div className={isNotCopied === true ? "isCopied inactive" : "isCopied active" }>Copied to clipboard.</div>
         </section>
-        <Link to="about-component" spy={true} smooth={true} offset={0} duration={600}>
+        <Link
+          to="about-component"
+          spy={true}
+          smooth={true}
+          offset={0}
+          duration={600}
+        >
           <div className="scroll-down">
             <FontAwesomeIcon
               icon={faChevronDown}
